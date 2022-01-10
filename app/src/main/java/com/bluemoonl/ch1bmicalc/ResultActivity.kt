c64524ab7c62e209a96b49fba1258f300958d493
BlueMoonL <79078980+BlueMoonL@users.noreply.github.com>
2022-01-10 20:09:52 +0900

    BMIcalc

---
+++ app/src/main/java/com/bluemoonl/ch1bmicalc/ResultActivity.kt
@@ -0,0 +1,33 @@
+package com.bluemoonl.ch1bmicalc
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.text.DecimalFormat
+import kotlin.math.pow
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val height = intent.getDoubleExtra("height", 0.0)
+        val weight = intent.getDoubleExtra("weight", 0.0)
+
+        val calc = DecimalFormat("##00.00")
+        val bmi = calc.format(weight / (height / 100).pow(2.0)).toDouble()
+        val resultText = when {
+            bmi >= 35.0 -> "고도 비만"
+            bmi >= 30.0 -> "중정도 비만"
+            bmi >= 25.0 -> "경도 비만"
+            bmi >= 23.0 -> "과체중"
+            bmi >= 18.5 -> "정상체중"
+            else -> "저체중"
+        }
+
+        val resultValueTextView = findViewById<TextView>(R.id.bmiResultTextView)
+        val resultStringTextView = findViewById<TextView>(R.id.resultTextView)
+
+        resultValueTextView.text = bmi.toString()
+        resultStringTextView.text = resultText
+    }
+}
